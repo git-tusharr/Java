470b5b993f74a2df668f7f0a5404fc77a36e5f22
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-05-27 06:02:12 +0530

    practiced

--- LoopsInJava/Pattern.java
+++ LoopsInJava/Pattern.java
@@ -1,274 +1,372 @@
-public class Pattern {
-    public static void main(String[] args)
-    {
-            // right angle triangle
+// public class Pattern {
+//     public static void main(String[] args)
+//     {
+//             // right angle triangle
 
 
-            //    for(int r=1;r<=5;++r)
+//                for(int r=1;r<=5;++r)
+//                {
+//                 for(int c=1;c<=5;++c)
+//                 {
+//                     if(c<=r)
+//                     {
+//                         System.out.print("*");
+//                     }
+//                     else
+//                     {
+//                         System.out.print("  ");
+//                     }
+//                 }
+//                 System.out.println();
+//                }
+
+
+
+            //    public class Pattern
             //    {
-            //     for(int c=1;c<=5;++c)
+            //     public static void main (String [] args)
             //     {
-            //         if(c<=r)
+            //         for (int r=1;r<=5;++r)
             //         {
-            //             System.out.print("*");
+            //             for (int c=1;c<=5;++c)
+            //             {
+            //                 if(c<=r) 
+            //                 {
+            //                     System.out.print("*");
+            //                 }
+            //                 else
+            //                 {
+            //                     System.out.print(" ");
+            //                 }
+            //             }
+            //             System.out.println(); 
             //         }
-            //         else
-            //         {
-            //             System.out.print("  ");
-            //         }
+                    
             //     }
-            //     System.out.println();
             //    }
 
-
-            // // Inverted left angle triangle
+//             // // Inverted left angle triangle
                 
                 
-            //   for(int r=1;r<=5;++r)
-            //    {
-            //     for(int c=1;c<=5;++c)
-            //     {
-            //         if(c>=r)
-            //         {
-            //             System.out.print("*");
-            //         }
-            //         else
-            //         {
-            //             System.out.print(" ");
-            //         }
-            //     }
-            //     System.out.println();
-            //    }
-
-            // left angle triangle
-
-            //   for(int r=1;r<=5;++r)
-            //    {
-            //     for(int c=1;c<=5;++c)
-            //     {
-            //         if(c>=6-r)
-            //         {
-            //             System.out.print("*");
-            //         }
-            //         else
-            //         {
-            //             System.out.print(" ");
-            //         }
-            //     }
-            //     System.out.println();
-            //    }
+//             //   for(int r=1;r<=5;++r)
+//             //    {
+//             //     for(int c=1;c<=5;++c)
+//             //     {
+//             //         if(c>=r)
+//             //         {
+//             //             System.out.print("*");
+//             //         }
+//             //         else
+//             //         {
+//             //             System.out.print(" ");
+//             //         }
+//             //     }
+//             //     System.out.println();
+//             //    }
 
 
-            // inverted right angle triangle
+//             // left angle triangle
 
-            //    for(int r=1;r<=5;++r)
-            //     {
-            //         for(int c=1;c<=5;++c)
-            //         {
-            //             if(c<=6-r)
-            //             {
-            //                 System.out.print("*");
-            //             }
-            //             else
-            //             {
-            //                 System.out.print(" ");
-            //             }
-            //         }
-            //         System.out.println();
-
-            //     }
-
-
-    //         for(int r=1;r<=3;r++)
-    // {
-    //     for(int c=1;c<=r;c++)
-
-    //     {
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
+//             //   for(int r=1;r<=5;++r)
+//             //    {
+//             //     for(int c=1;c<=5;++c)
+//             //     {
+//             //         if(c>=6-r)
+//             //         {
+//             //             System.out.print("*");
+//             //         }
+//             //         else
+//             //         {
+//             //             System.out.print(" ");
+//             //         }
+//             //     }
+//             //     System.out.println();
+//             //    }
 
 
-    // }   
+                // public class Pattern
+                // {
+                //     public static void main (String []args)
+                //     {
+                //         for(int r=1;r<=5;r++)
+                //         {
+                //             for(int c=1;c<=5;c++)
+                //             {
+                //                 if(c>=6-r)
+                //                 {
+                //                     System.err.print("*");
+                //                 }
+                //                 else
+                //                 {
+                //                     System.out.print(" ");
+                //                 }
+                //             }
+                //             System.out.println();
+                //         }
+                //     }
+                // }
+
+
+//             // inverted right angle triangle
+
+//             //    for(int r=1;r<=5;++r)
+//             //     {
+//             //         for(int c=1;c<=5;++c)
+//             //         {
+//             //             if(c<=6-r)
+//             //             {
+//             //                 System.out.print("*");
+//             //             }
+//             //             else
+//             //             {
+//             //                 System.out.print(" ");
+//             //             }
+//             //         }
+//             //         System.out.println();
+
+//             //     }
+
+
+//     //         for(int r=1;r<=3;r++)
+//     // {
+//     //     for(int c=1;c<=r;c++)
+
+//     //     {
+//     //         System.out.print("*");
+//     //     }
+//     //     System.out.println();
+
+
+//     // }   
     
     
 
 
-    // Pyramid pattern
+//     // Pyramid pattern
 
-    //   for(int r=1;r<=5;++r)
-    //             {
-    //                 for(int c=1;c<=9;++c)
-    //                 {
-    //                     if(c>=6-r && c<=4+r)
-    //                     {
-    //                         System.out.print("*");
-    //                     }
-    //                     else
-    //                     {
-    //                         System.out.print(" ");
-    //                     }
-    //                 }
-    //                 System.out.println();
+//     //   for(int r=1;r<=5;++r)
+//     //             {
+//     //                 for(int c=1;c<=9;++c)
+//     //                 {
+//     //                     if(c>=6-r && c<=4+r)
+//     //                     {
+//     //                         System.out.print("*");
+//     //                     }
+//     //                     else
+//     //                     {
+//     //                         System.out.print(" ");
+//     //                     }
+//     //                 }
+//     //                 System.out.println();
 
-    //             }
-
-
+//     //             }
 
 
-     //inverted pyramid pattern
+
+
+//      //inverted pyramid pattern
      
-        // for(int r=1;r<=5;++r)
-        //         {
-        //             for(int c=1;c<=9;++c)
-        //             {
-        //                 if(c>=r && c<=10-r)
-        //                 {
-        //                     System.out.print("*");
-        //                 }
-        //                 else
-        //                 {
-        //                     System.out.print(" ");
-        //                 }
-        //             }
-        //             System.out.println();
+//         // for(int r=1;r<=5;++r)
+//         //         {
+//         //             for(int c=1;c<=9;++c)
+//         //             {
+//         //                 if(c>=r && c<=10-r)
+//         //                 {
+//         //                     System.out.print("*");
+//         //                 }
+//         //                 else
+//         //                 {
+//         //                     System.out.print(" ");
+//         //                 }
+//         //             }
+//         //             System.out.println();
 
-        //         }
+//         //         }
 
 
     
-                // Diamond pattern
+//                 // Diamond pattern
 
-                //     int a=0;
-                //   for(int r=1;r<=9;++r)
-                // {
-                //     a=(r<=5)?++a:--a;
+//                 //     int a=0;
+//                 //   for(int r=1;r<=9;++r)
+//                 // {
+//                 //     a=(r<=5)?++a:--a;
 
-                //     for(int c=1;c<=9;++c)
-                //     {
-                //         if(c>=6-a && c<=4+a)
-                //         {
-                //             System.out.print("*");
-                //         }
-                //         else
-                //         {
-                //             System.out.print(" ");
-                //         }
-                //     }
-                //     System.out.println();
+//                 //     for(int c=1;c<=9;++c)
+//                 //     {
+//                 //         if(c>=6-a && c<=4+a)
+//                 //         {
+//                 //             System.out.print("*");
+//                 //         }
+//                 //         else
+//                 //         {
+//                 //             System.out.print(" ");
+//                 //         }
+//                 //     }
+//                 //     System.out.println();
 
-                // }
-
-
-
-            //     int a=0;
-
-            //     for(int r=1;r<=9;++r)
-            //     {
-            //     a=(r<=5)?++a:--a;
-            //     for(int c=1;c<=9;++c)
-            //     {
-            //         if(c>=6-a && c<=4+a)
-            //         {
-            //             System.out.print("*");
-            //         }
-            //         else
-            //         {
-            //             System.out.print(" ");
-            //         }
-            //     }
-            //     System.out.println();
-            // }
-
-
-            // Print 1,2,3,4,5........   
-                //  int a=0;
-                //   for(int r=1;r<=5;++r)
-                // {
-
-                //     for(int c=1;c<=5;++c)
-                //     {
-                //         if(c<=r)
-                //         {
-                //             System.out.print(" "+a++);
-                //         }
-                //         else
-                //         {
-                //             System.out.print(" ");
-                //         }
-                //     }
-                //     System.out.println();
-
-                // }
+//                 // }
 
 
 
-                // print a,b,c,d,e,f.......
+//             //     int a=0;
 
-                //  char a='a';
-                //   for(int r=1;r<=5;++r)
-                // {
-
-                //     for(int c=1;c<=5;++c)
-                //     {
-                //         if(c<=r)
-                //         {
-                //             System.out.print(a++);
-                //         }
-                //         else
-                //         {
-                //             System.out.print(" ");
-                //         }
-                //     }
-                //     System.out.println();
-
-                // }
+//             //     for(int r=1;r<=9;++r)
+//             //     {
+//             //     a=(r<=5)?++a:--a;
+//             //     for(int c=1;c<=9;++c)
+//             //     {
+//             //         if(c>=6-a && c<=4+a)
+//             //         {
+//             //             System.out.print("*");
+//             //         }
+//             //         else
+//             //         {
+//             //             System.out.print(" ");
+//             //         }
+//             //     }
+//             //     System.out.println();
+//             // }
 
 
+//             // Print 1,2,3,4,5........   
+//                 //  int a=0;
+//                 //   for(int r=1;r<=5;++r)
+//                 // {
 
-                // print a,bb,ccc,dddd,..........
+//                 //     for(int c=1;c<=5;++c)
+//                 //     {
+//                 //         if(c<=r)
+//                 //         {
+//                 //             System.out.print(" "+a++);
+//                 //         }
+//                 //         else
+//                 //         {
+//                 //             System.out.print(" ");
+//                 //         }
+//                 //     }
+//                 //     System.out.println();
 
-                //  char a='a';
-                //   for(int r=1;r<=5;++r)
-                // {
-
-                //     for(int c=1;c<=5;++c)
-                //     {
-                //         if(c<=r)
-                //         {
-                //             System.out.print((char)(r+64));  //typecast to character
-                //         }
-                //         else
-                //         {
-                //             System.out.print(" ");
-                //         }
-                //     }
-                //     System.out.println();
-
-                // }
+//                 // }
 
 
+                    // public class Pattern
+                    // {
+                    //     public static void main (String[] args)
+                    //     {
+                    //         int a=0;
+                    //         for(int r=1;r<=5;r++)
+                    //         {
+                    //             for(int c=1;c<=5;c++)
+                    //             {
+                    //                 if(c<=r)
+                    //                 {
+                    //                 System.out.print(" "+ a++);
+                    //                 }
+                    //                 else
+                    //                 {
+                    //                     System.out.print("");
+                    //                 }
+                    //             }
+                    //             System.out.println();
+                                
+                    //         }
+                    //     }
+                    // }
 
-                // A,AB,ABC,ABCD,ABCDE........
 
-                char a='a';
-                  for(int r=1;r<=5;++r)
-                {
 
-                    for(int c=1;c<=5;++c)
-                    {
-                        if(c<=r)
-                        {
-                            System.out.print((char)(c+64));  //typecast to character
-                        }
-                        else
-                        {
-                            System.out.print(" ");
-                        }
-                    }
-                    System.out.println();
+// //                 // print a,b,c,d,e,f.......
 
-                }
+// //                 //  char a='a';
+// //                 //   for(int r=1;r<=5;++r)
+// //                 // {
+
+// //                 //     for(int c=1;c<=5;++c)
+// //                 //     {
+// //                 //         if(c<=r)
+// //                 //         {
+// //                 //             System.out.print(a++);
+// //                 //         }
+// //                 //         else
+// //                 //         {
+// //                 //             System.out.print(" ");
+// //                 //         }
+// //                 //     }
+// //                 //     System.out.println();
+
+// //                 // }
+
+// public class Pattern 
+// {
+//     public static void main(String[] args)
+//     {
+//         char a='a';
+//         for(int r=1;r<=5;++r)
+//                 {
+//                     for(int c=1;c<=5;++c)
+//                     {
+//                         if(c<=r)
+//                         {
+//                             System.out.print(a++);
+//                         }
+//                         else
+//                         {
+//                             System.out.print(" ");
+//                         }
+//                     }
+//                     System.out.println();
+//                 }
+//     }
+    
+// }
+
+
+
+
+//                 // print a,bb,ccc,dddd,..........
+
+//                 //  char a='a';
+//                 //   for(int r=1;r<=5;++r)
+//                 // {
+
+//                 //     for(int c=1;c<=5;++c)
+//                 //     {
+//                 //         if(c<=r)
+//                 //         {
+//                 //             System.out.print((char)(r+64));  //typecast to character
+//                 //         }
+//                 //         else
+//                 //         {
+//                 //             System.out.print(" ");
+//                 //         }
+//                 //     }
+//                 //     System.out.println();
+
+//                 // }
+
+
+
+//                 // A,AB,ABC,ABCD,ABCDE........
+
+//                 // char a='a';
+//                 //   for(int r=1;r<=5;++r)
+//                 // {
+
+//                 //     for(int c=1;c<=5;++c)
+//                 //     {
+//                 //         if(c<=r)
+//                 //         {
+//                 //             System.out.print((char)(c+64));  //typecast to character
+//                 //         }
+//                 //         else
+//                 //         {
+//                 //             System.out.print(" ");
+//                 //         }
+//                 //     }
+//                 //     System.out.println();
+
+//                 // }
             
 
 
@@ -298,5 +396,5 @@ public class Pattern {
 
 
 
-    }
-}
+//     }
+// }
