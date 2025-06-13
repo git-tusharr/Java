73a861aa9e6d77b3be59ac4fa4ac9693369a34ce
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-13 23:36:30 +0530

    strings

---
+++ Strings/One.java
@@ -0,0 +1,9 @@
+// Strings :-
+
+-> Array of characters
+    
+    char ch[]={'t','u','s','h','a','r'}
+    string 
+
+-> constructors,functions(append,firstindex etc)
+-> Strings are immutable beacause changing one values in scp is will change all values sharing that value
