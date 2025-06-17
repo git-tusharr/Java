937e249e096c5a65e79c1cb28ca4e449f65fe9e8
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-17 17:55:51 +0530

    Case swap

--- Strings/StringsQuestions.java
+++ Strings/StringsQuestions.java
@@ -24,10 +24,84 @@
 //             left++;
 //         }
 
+//         for(char a:arr){
+//             System.out.println(a);
+//         }
+//     }
+
+//     public static void main (String []args){
+
+//         String str="abcd";
+//         rev(str);
+//     }
+// }
 
 
 
 
+// class StringsQuestions{
+
+//     static void reverse(String str){
+
+//         char arr[]=new char[str.length()];        
+//         for(int i=0;i<str.length();i++){
+//             arr[i]=str.charAt(i);
+//         }
+
+
+//         int left=0;
+//         int right =str.length()-1;
+
+//         while(left<right){
+//             char temp =arr[left];
+//             arr[left]=arr[right];
+//             arr[right]=temp;
+
+//             right--;
+//             left++;
+//         }
+
+
+//         for(char a:arr){
+//             System.out.println(a);
+//         }
+
+
+//     }
+
+//     public static void main(String []args){
+//         String str="abcde";
+//         reverse(str);
+
+//     }
+// }
+
+
+
+
+// class StringsQuestions{
+
+//     static void rev(String str){
+//         char arr[]=new char [str.length()];
+
+//         for(int i=0;i<str.length();++i){
+//             arr[i]=str.charAt(i);  
+
+//         }
+
+//         int right =str.length()-1;
+//         int left=0;
+
+// for(int i=0;i<str.length()/2;i++){
+//     char temp=arr[left];
+//     arr[left]=arr[right];
+//     arr[right]=temp;
+    
+//     left++;
+//     right--;
+
+
+//     }
 
 //         for(char a:arr){
 //             System.out.println(a);
@@ -38,13 +112,12 @@
 
 //         String str="abcd";
 //         rev(str);
-
-
-
-
-
 //     }
 // }
+
+
+
+
 
 
 
@@ -92,38 +165,121 @@
 
 
 
+// class StringsQuestions{
+
+//     static boolean palindrome(String str){
+
+//         int left=0;
+//         int right =str.length()-1;
+        
+
+//         while(left<right){
+//             if(str.charAt(right)!=str.charAt(left)){
+//                 return false;
+//             }
+
+//         left++;
+//         right--;
+            
+//         }
+
+        
+
+//         return true;
+//     }
+
+//     public static void main (String []args){
+
+//         String str="naman";
+//         if(palindrome(str)){
+//             System.out.println("Yes its palindrome");
+//         }
+
+//         else{
+//             System.out.println("no its not palindrome");
+//         }
+//     }
+// }
+
+
+
+// class StringsQuestions{
+    
+    
+
+// static boolean palindrome(String str){
+    
+//     int left=0;
+//     int right=str.length()-1;
+    
+//     while(left<right){
+//         if(str.charAt(left)!=str.charAt(right)){
+//             return false;
+//         }
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+
+//     public static void main(String []args){
+//         String str="ABCBa";
+
+//         if(palindrome(str)){
+//             System.out.println("it is a palindrome");
+//         }
+//         else{
+//             System.out.println("it is not a palindrome");
+//         }
+
+//     }
+
+// }
+
+
+
+
+
+// Ques. if in lowercase convert in uppercase and vice versa
+
 class StringsQuestions{
 
-    static boolean palindrome(String str){
+    
 
-        int left=0;
-        int right =str.length()-1;
-        
+    public static void Convert(String str){
 
-        while(left<right){
-            if(str.charAt(right)!=str.charAt(left)){
-                return false;
-            }
+    char[] arr=new char[str.length()];
+    for(int i=0;i<str.length();i++){
 
-        left++;
-        right--;
-            
+         arr[i]=str.charAt(i);
+
+    }
+    for(int i=0;i<str.length();i++){
+        if(arr[i]>='A' && arr[i]<='Z'){
+            arr[i]=(char)(arr[i]+32);
         }
-
-        
-
-        return true;
+        else if(arr[i]>='a' && arr[i]<='z'){
+            arr[i]=(char)(arr[i]-32);
+        }
     }
 
-    public static void main (String []args){
-
-        String str="naman";
-        if(palindrome(str)){
-            System.out.println("Yes its palindrome");
+    for (char c : arr) {
+            System.out.print(c);
         }
 
-        else{
-            System.out.println("no its not palindrome");
-        }
+
+
+    }
+
+    public static void main(String []args){
+        String str="ABCD";
+        Convert(str);
     }
 }
+
+
+
+
+
+
+
