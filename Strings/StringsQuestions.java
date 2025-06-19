783e498fb05e7364c5ac2f6e6025ea6595847aaa
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-19 07:50:16 +0530

    Strings questions

--- Strings/StringsQuestions.java
+++ Strings/StringsQuestions.java
@@ -240,42 +240,150 @@
 
 
 
-// Ques. if in lowercase convert in uppercase and vice versa
+// 
 
-class StringsQuestions{
+
+
+//     public static void Convert(String str){
+
+//     char[] arr=new char[str.length()];
+//     for(int i=0;i<str.length();i++){
+
+//          arr[i]=str.charAt(i);
+
+//     }
+//     for(int i=0;i<str.length();i++){
+//         if(arr[i]>='A' && arr[i]<='Z'){
+//             arr[i]=(char)(arr[i]+32);
+//         }
+//         else if(arr[i]>='a' && arr[i]<='z'){
+//             arr[i]=(char)(arr[i]-32);
+//         }
+//     }
+
+//     for (char c : arr) {
+//             System.out.print(c);
+//         }
+
+
+
+//     }
+
+//     public static void main(String []args){
+//         String str="ABCD";
+//         Convert(str);
+//     }
+// }
+
+
+
+
+
+
+// Ques. convert uppercase to lowercase 
+
+// class StringsQuestions{
 
     
 
-    public static void Convert(String str){
 
-    char[] arr=new char[str.length()];
-    for(int i=0;i<str.length();i++){
+// public class StringsQuestions{
 
-         arr[i]=str.charAt(i);
+//     public static void ToLowercase(String str){
+//         char[] arr=new char[str.length()];
 
-    }
-    for(int i=0;i<str.length();i++){
-        if(arr[i]>='A' && arr[i]<='Z'){
-            arr[i]=(char)(arr[i]+32);
-        }
-        else if(arr[i]>='a' && arr[i]<='z'){
-            arr[i]=(char)(arr[i]-32);
-        }
-    }
+//         for(int i=0;i<str.length();i++){
+//             arr[i]=str.charAt(i);
+//         }
 
-    for (char c : arr) {
-            System.out.print(c);
-        }
+//         for(int i=0;i<str.length();i++){
+//             if(arr[i]>='A' && arr[i]<='z'){
+//                 arr[i]=(char)(arr[i]+32);
+//             }
+//         }
+//             for (char c : arr) {
+//             System.out.print(c);
+//             }
+         
+
+//     }
+//     public static void main(String []args){
+
+//         String str="ABCD";
+//         ToLowercase(str);
+
+//     }
+// }
 
 
 
-    }
 
-    public static void main(String []args){
-        String str="ABCD";
-        Convert(str);
-    }
-}
+// Ques- Converts lowercase to upper case
+
+
+// public class StringsQuestions{
+
+//     public static void ToLowercase(String str){
+//         char[] arr=new char[str.length()];
+
+//         for(int i=0;i<str.length();i++){
+//             arr[i]=str.charAt(i);
+//         }
+
+//         for(int i=0;i<str.length();i++){
+//             if(arr[i]>='A' && arr[i]<='z'){
+//                 arr[i]=(char)(arr[i]-32);
+//             }
+//         }
+//             for (char c : arr) {
+//             System.out.print(c);
+//             }
+         
+
+//     }
+
+
+
+
+//     public static void main(String []args){
+
+//         String str="abcd";
+//         ToLowercase(str);
+
+//     }
+// }
+
+
+
+
+// Ques - printing array values using comma seperator
+
+
+// public class StringsQuestions{
+//     public static void main(String [] args){
+//         int arr[]=new int[]{1,2,3,4};
+
+
+        
+//     for(int k=0;k<arr.length;k++)
+//          {
+            
+
+
+          
+//             System.out.print(arr[k]);
+            
+            
+        
+//          }
+//     }
+// }
+
+
+
+
+// Que - Subsequence
+
 
 
 
