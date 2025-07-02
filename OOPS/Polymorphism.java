da51f508ee0b5c803a80a7a2ed028ab8a0565674
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-02 09:05:31 +0530

    comile time and run time

--- OOPS/Polymorphism.java
+++ OOPS/Polymorphism.java
@@ -1,10 +1,52 @@
-// Function overLoading
+// Function overLoading Or Compile time polymorphism
+
+// class Polymorphism{
+
+//     void sum(){
+//         System.out.println("sum");
+//     }
+
+//      void sum(int a){
+//         System.out.println("sum"+" "+a);
+//     }
+
+//     public static void main(String args[]){
+//         Polymorphism obj =new Polymorphism();
+//         obj.sum();
+//         obj.sum(2);
+//     }
+
+// }
 
 
 
+// Method Overriding Or Run time polymorphism through classes
 
-// Method Overriding
+class Animal{
+    void sound(){
+        System.out.println("Animal Sound");
+    }
+}
+
+class cat extends Animal{
+    void sound(){
+        System.out.println("Cat Sound");
+    }
+}
+
+class dog extends Animal{
+    void sound(){
+        System.out.println("dog Sound");
+    }
+}
 
 class Polymorphism{
-    
+    public static void main (String []args){
+        Animal obj =new cat();
+        obj.sound();
+
+        Animal obj1 =new dog();
+        obj1.sound();
+    }
 }
+
