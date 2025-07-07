7a7d93a6c96b2ac8f88239c97399262d8ac396f1
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-08 04:01:55 +0530

    Practiced constructor

---
+++ OOPS/Constructor.java
@@ -0,0 +1,138 @@
+//  Constructor overloading
+
+// class Constructor{
+
+//     String a;
+//     int b;
+//     Constructor(){
+//         System.out.println("Default constructor");
+//     }
+
+//     Constructor(int a){
+//         System.out.println("Overloaded constructor");
+//     }
+
+    
+// public static void main(String srgs[]){
+
+//     Constructor obj=new Constructor(10);
+// }
+
+// }
+
+
+// class Constructor{
+//     int b;
+
+//     Constructor(){
+//         System.out.println("This is default constructor");
+//     }
+
+//     Constructor(int a){
+//         System.out.println("This is overloaded constructor");
+//     }
+
+//     public static void main(String args[]){
+//         Constructor obj=new Constructor(10);
+//     }
+// }
+
+
+
+
+
+
+
+//   Shallow copy / Deep copy
+
+// Shallow copy constructor - shared memory
+
+
+// class Constructor{
+
+//     int []b;
+//     Constructor(int b[]){
+//         this.b=b;
+//         System.out.println("Parameterized constructor"+" "+this.b[0]);
+//     }
+ 
+//     Constructor(Constructor obj1){
+//         this.b=obj1.b;
+//         System.out.println("copy constructor"+" "+this.b[0]);
+//     }
+
+    
+// public static void main(String args[]){
+//     int arr[]={15};
+
+//     Constructor obj1=new Constructor(arr);
+
+//     Constructor obj2=new Constructor(obj1);
+// }
+
+// }
+
+// class Constructor{
+//     int b[];
+//     Constructor(int b[]){
+//         this.b=b;
+//         System.out.println("Parameterized constructor"+" "+this.b[0]);
+//     }
+
+//     Constructor(Constructor obj1){
+//         this.b=obj1.b;
+//         System.out.println("copy constructor"+" "+this.b[0]);
+//     }
+
+
+
+// public static void main(String []args){
+//     int arr[]={15};
+//     Constructor obj =new Constructor(arr);
+//     Constructor obj1= new Constructor(obj);
+// }
+// }
+
+
+
+
+
+
+
+
+// // Deep copy constructor
+
+// class Constructor{
+
+//     int []b;
+//     Constructor(int b[]){
+//         this.b=b;
+//     }
+ 
+//     Constructor(Constructor obj){
+
+//         this.b=new int [obj.b.length];
+//         this.b[0]=obj.b[0];
+//     }
+
+// public static void main(String args[]){
+//     int arr[]={10};
+
+//     Constructor obj1=new Constructor(arr);
+//     Constructor obj2=new Constructor(obj1);
+
+//     obj1.b[0]=29;
+
+//     System.out.println(obj1.b[0]);
+//     System.out.println(obj2.b[0]);
+// }
+
+// }
+
+
+
+
+
+
+
+
