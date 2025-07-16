c7a9d22719bb45aa45b4552cfc6806894cb61def
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-16 22:18:52 +0530

    Linked list (node)

--- DSA Java/Node (linked list)/One.java
+++ DSA Java/Node (linked list)/One.java
@@ -1,1 +1,31 @@
+ class Node{
+    int value;
+    Node next;
+
+    Node(int value){
+        this.value=value;
+        this.next=null;
+    }
+
+ }
+
+    class One{
+        public static void main(String args[]){
+            Node node1 = new Node(10);
+            Node node2 = new Node(11);
+            Node node3 = new Node(12);
+
+            node1.next=node2;
+            node2.next=node3;
+            node3.next=null;
+
+            Node temp;
+            temp=node1; 
+
+            while (temp!=null){
+                System.out.println(temp.value);
+                temp = temp.next;
+            }
+        }
+    }
  
