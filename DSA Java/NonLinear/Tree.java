8a4e0e7d5af5addec57afdbdf1b8ba11c4173e19
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-20 09:40:17 +0530

    non linear ds

---
+++ DSA Java/NonLinear/Tree.java
@@ -0,0 +1,26 @@
+class Timpl{
+    int data;
+    Timpl left;
+    Timpl right;
+
+    Timpl(int data){
+        this.data=data;
+        left=null;
+        right=null;
+    }
+
+}
+
+public class Tree{
+    public static void main(String args[]){
+        Timpl obj=new Timpl(10);
+        obj.left =new Timpl(20);
+        obj.right =new Timpl(30);
+        obj.left.right =new Timpl(30);
+
+        System.out.println("obj root data "+obj.data);
+        System.out.println("obj left  "+obj.left.data);
+        System.out.println("obj right "+obj.right.data);
+        System.out.println("obj left right "+obj.left.right.data);
+    }
+}
