9beda9b54b99c6bb2a9576cda8c27e3cd4bc2b88
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-07-21 12:06:37 +0530

    tree practice

--- DSA Java/NonLinear/Tree.java
+++ DSA Java/NonLinear/Tree.java
@@ -1,26 +1,56 @@
+// class Timpl{
+//     int data;
+//     Timpl left;
+//     Timpl right;
+
+//     Timpl(int data){
+//         this.data=data;
+//         left=null;
+//         right=null;
+//     }
+
+// }
+
+// public class Tree{
+//     public static void main(String args[]){
+//         Timpl obj=new Timpl(10);
+//         obj.left =new Timpl(20);
+//         obj.right =new Timpl(30);
+//         obj.left.right =new Timpl(30);
+
+//         System.out.println("obj root data "+obj.data);
+//         System.out.println("obj left  "+obj.left.data);
+//         System.out.println("obj right "+obj.right.data);
+//         System.out.println("obj left right "+obj.left.right.data);
+//     }
+// }
+
+
+
+
 class Timpl{
-    int data;
-    Timpl left;
-    Timpl right;
+    int data; //root data
+    Timpl left; // Root ka child (left)
+    Timpl right; // Root ka child (right)
 
     Timpl(int data){
         this.data=data;
-        left=null;
         right=null;
+        left=null;
     }
-
 }
 
-public class Tree{
-    public static void main(String args[]){
-        Timpl obj=new Timpl(10);
-        obj.left =new Timpl(20);
-        obj.right =new Timpl(30);
-        obj.left.right =new Timpl(30);
+class Tree{
 
-        System.out.println("obj root data "+obj.data);
-        System.out.println("obj left  "+obj.left.data);
-        System.out.println("obj right "+obj.right.data);
-        System.out.println("obj left right "+obj.left.right.data);
+    public static void main(String args []){
+    Timpl obj=new Timpl(10);
+    obj.left=new Timpl(20);
+    obj.right=new Timpl(30);
+
+    System.out.println(obj.data);
+    System.out.println(obj.left.data);
+    System.out.println(obj.right.data);
     }
+
+
 }
