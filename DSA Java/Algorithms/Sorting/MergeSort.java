6c027dabf8cb24910fd564c3ae82d0878efd9879
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-08-26 14:16:54 +0530

    merge sort

---
+++ DSA Java/Algorithms/Sorting/MergeSort.java
@@ -0,0 +1,75 @@
+
+
+
+
+
+class MergeSort{
+    static int[] divide(int arr[]){
+        if (arr.length<=1) return arr;
+        int mid = arr.length/2;
+
+        int left[] = new int[mid];
+        int right[] = new int[arr.length-mid];
+
+        for (int i=0;i<mid;i++){
+            left[i]=arr[i];
+        }
+        for (int i=mid;i<arr.length;i++){
+            right[i-mid]=arr[i];
+        }
+
+        left= divide(left);
+        right= divide(right);
+
+        return merge(left,right);
+    }
+    static int[] merge(int left[],int right[]){
+        int result[]=new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                result[k++]=left[i++];
+            }
+            else{
+                result[k++]=right[j++];
+            }
+
+            
+            
+        }
+
+                while(i<left.length)
+            {
+                result[k++]=left[i++];
+            }
+
+
+                 while(j<right.length)
+            {
+                result[k++]=right[j++];
+            }
+        return result;
+    }
+
+        
+
+    
+    public static void main(String arge []){
+
+        int arr[] = {12,7,5,4,3,2};
+        int sorted[]=divide(arr);
+
+        for(int i=0;i<sorted.length;++i){
+            System.out.println(sorted[i]);
+        }
+        
+        }
+
+    
+}
+    
+    
+    
--- DSA Java/Algorithms/Sorting/Recursion.java
+++ DSA Java/Algorithms/Sorting/Recursion.java
@@ -1,5 +1,5 @@
 // class Recursion{
-
+ 
 
 //     static int fact(int number)
 //     {
@@ -16,9 +16,63 @@
 // }
 
 
+
+
+// Fibonacci series by recursion
+
+
+// class Recursion{
+//     static int fibonacci(int n){
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+
+//         return fibonacci(n-1)+fibonacci(n-2);
+//     }
+//     public static void main(String arge []){
+//         int number =10;
+//         System.out.println("Fibonacci Series using recursion:");
+
+//         for (int i = 0; i < number; i++) {
+//         System.out.println(fibonacci(i)); 
+//         }
+//     }
+// }
+
+
+
+
+
+
+
 class Recursion{
-    static int divide(int arr[]){
-        if()
+    static void divide(int arr[]){
+        if (arr.length<=1) return;
+        int mid = arr.length/2;
+
+        int left[] = new int[mid];
+        int right[] = new int[arr.length-mid];
+
+        for (int i=0;i<mid;i++){
+            left[i]=arr[i];
+        }
+        for (int i=mid;i<arr.length;i++){
+            right[i-mid]=arr[i];
+        }
+
+        divide(left);
+        divide(right);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+            System.out.println();
 
     }
-}
+    public static void main(String arge []){
+
+        int arr[] = {12,7,5,4,3,2};
+
+        divide(arr);
+        
+        }
+    }
