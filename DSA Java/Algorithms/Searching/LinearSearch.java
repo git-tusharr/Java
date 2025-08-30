403166327831b60aaa24b3aa00e9f4e8f4224e8a
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-08-30 14:43:45 +0530

    leanear search practice

---
+++ DSA Java/Algorithms/Searching/LinearSearch.java
@@ -0,0 +1,30 @@
+class LinearSearch{
+
+    static int Ls(int arr[],int target)
+    {
+        for(int i=0;i<arr.length;++i)
+        {
+            if(arr[i]==target)
+            {
+                return 1;
+            } 
+        }
+        return -1;
+    }
+
+
+    public static void main(String str[])
+ {
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int target=5;
+
+    if(Ls(arr,target)==1)
+    {
+        System.out.println("Yes");
+    }
+    else
+    {
+        System.out.println("No");
+    }
+ }
+}
