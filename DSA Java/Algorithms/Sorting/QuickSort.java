3f97db3032bd2a1b2552d7a348aef6d314e838b6
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-08-30 14:54:04 +0530

    practiced quick sort

--- DSA Java/Algorithms/Sorting/QuickSort.java
+++ DSA Java/Algorithms/Sorting/QuickSort.java
@@ -13,9 +13,10 @@ class QuickSort{
         int i=low;
         for(int j=low;j<high;++j){
             if(arr[j]<pivot){
-
-                 
-                
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
             }
         }
         int temp=arr[i];
@@ -37,3 +38,57 @@ class QuickSort{
 
         }
 }
+
+
+// class QuickSort{
+
+//     static void qs(int arr[],int low,int high)
+//     {
+//         if(low<high)
+//         {
+//         int pivotIndex=partion(arr,low,high);
+
+//         qs(arr,low,pivotIndex-1);
+//         qs(arr,pivotIndex+1,high);
+
+//         }
+        
+//     }
+
+//     static int partion(int arr[],int low,int high)
+//     {
+//         int pivot=arr[high];
+//         int i=low;
+
+//         for(int j=low;j<=high;++j)
+//         {
+//             if(arr[j]<pivot)
+//             {
+//                 int temp =arr[i];
+//                 arr[i]=arr[j];
+//                 arr[j]=temp;
+//                 i++;
+//             }
+//         }
+
+//         int temp=arr[high];
+//         arr[high]=arr[i];
+//         arr[i]=temp;
+
+//         return i;
+
+//     }
+
+//     public static void main(String args[]){
+//         int arr[]={10,9,8,7,1,5};
+//         int low=0;
+//         int n=arr.length;
+
+//         qs(arr,low,n-1);
+//         for(int i:arr)
+//         {
+//              System.out.println(i);
+//         }
+
+//     }
+// }
