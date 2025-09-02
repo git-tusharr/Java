114b78898a2e5d9d12d4b569a49bc992b3e7b58d
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-09-02 18:07:22 +0530

    fibbonacci series by dp

---
+++ DSA Java/Algorithms/Searching/BinarySearch.java
@@ -0,0 +1,9 @@
+X
+if(arr[mid]==target) return mid;
+else if(target>arr[mid]) low-mid+1
+else high-mid-1;
+return -1;
+public static void main(String str
+int arr[]-(10,20,30,40,50,60);
+int target=10;
+System.out.println(Bs(target, arr)
