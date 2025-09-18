a28603e957aa3a8f49ca091fc61ae2525f20c17f
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-09-18 21:23:08 +0530

    reverse string practice

--- Strings/ReverseOfArray.java
+++ Strings/ReverseOfArray.java
@@ -2,34 +2,61 @@
 // Reversing of array by method ( .toCharArray() )
 
 
-class ReverseOfArray{
+// class ReverseOfArray{
 
-    static void rev(String str){
+//     static void rev(String str){
         
 
+//         char arr[]=str.toCharArray();
+
+//         int right =str.length()-1;
+//         int left=0;
+
+//         while(left<right){
+//             char temp=arr[left];
+//             arr[left]=arr[right];
+//             arr[right]=temp;
+
+//             right--;
+//             left++;
+//         }
+
+//         for(char a:arr){
+//             System.out.println(a);
+//         }
+//     }
+
+//     public static void main (String []args){
+
+//         String str="abcd";
+//         rev(str);
+//     }
+// }
+
+
+class ReverseOfArray{
+    static void rev(String str){
         char arr[]=str.toCharArray();
 
-        int right =str.length()-1;
+        int right=str.length()-1;
         int left=0;
 
         while(left<right){
+
             char temp=arr[left];
             arr[left]=arr[right];
             arr[right]=temp;
 
-            right--;
             left++;
+            right--;
         }
-
         for(char a:arr){
-            System.out.println(a);
-        }
+        System.out.println(a);
+    }
     }
 
-    public static void main (String []args){
-
-        String str="abcd";
+    public static void main(String args[]){
+        String str="abcde";
         rev(str);
     }
 }
-
