26df6b33a12a8682899677987b5acd846f917e8d
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-10-09 23:57:38 +0530

    linked liost

--- DSA Java/LinearDataStructure/Node (linked list)/FunctionsOfLinkedList.java
+++ DSA Java/LinearDataStructure/Node (linked list)/FunctionsOfLinkedList.java
@@ -24,6 +24,19 @@ import java.util.LinkedList;
 
 
 
+// public class FunctionsOfLinkedList {
+//   public static void main(String[] args) {
+//     LinkedList<String> cars = new LinkedList<String>();
+//     cars.add("Volvo");
+//     cars.add("BMW");
+//     cars.add("Ford");
+//     cars.add("Mazda");
+//     cars.sort(null);
+//     System.out.println(cars);
+//   }
+// }  
+
+
 public class FunctionsOfLinkedList {
   public static void main(String[] args) {
     LinkedList<String> cars = new LinkedList<String>();
