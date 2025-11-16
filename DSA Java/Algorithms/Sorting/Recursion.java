53798cc9752b753c757a297b4247e9177941ab0a
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-11-16 22:38:23 +0530

    reursion

--- DSA Java/Algorithms/Sorting/Recursion.java
+++ DSA Java/Algorithms/Sorting/Recursion.java
@@ -79,6 +79,21 @@
 
 
 
+// public class Recursion{
+//     static int factorial(int n){
+//         if (n==1) return 1;
+//         return n*factorial(n-1);
+//     }
+
+//     public static void main (String args[]){
+//         int result=factorial(4);
+//         System.out.print(result);
+//     }
+// }
+
+
+
+
 public class Recursion{
     static int factorial(int n){
         if (n==1) return 1;
