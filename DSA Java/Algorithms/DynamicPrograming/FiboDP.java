4e90cd92404c500e9e9cbe9a3e8a4a490af1b79e
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-04-29 23:13:42 +0530

    java revision

--- DSA Java/Algorithms/DynamicPrograming/FiboDP.java
+++ DSA Java/Algorithms/DynamicPrograming/FiboDP.java
@@ -26,6 +26,27 @@
 
 // Bottom-top approach
 
+// class FiboDP{
+//     static int fibo(int n){
+//         if(n<=1) return n;
+
+//         int dp[]=new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+
+//         for(int i=2;i<dp.length;++i){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//             return dp[n];
+//     }
+
+//     public static void main (String args[]){
+//         int n=6;
+//         System.out.println(fibo(n));
+//     }
+// }
+
+
 class FiboDP{
     static int fibo(int n){
         if(n<=1) return n;
