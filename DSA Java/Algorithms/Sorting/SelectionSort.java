bd65f7b3831429e8be7d9be1329ad36da44c950c
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-05-03 22:51:18 +0530

    selection sort

--- DSA Java/Algorithms/Sorting/SelectionSort.java
+++ DSA Java/Algorithms/Sorting/SelectionSort.java
@@ -29,6 +29,34 @@
 
 
 
+// class SelectionSort{
+//     public static void main(String args[]){
+//         int arr[]={96,88,45,109,39};
+//         int length=arr.length;
+
+//         for(int i=0;i<length-1;i++)
+//         {
+//             int min=i;
+//             for(int j=i+1;j<length;j++)
+//             {
+//                 if(arr[j]<arr[min])
+//                 {
+//                     min=j;
+//                 }
+//             }
+//             int temp=arr[min];
+//             arr[min]=arr[i];
+//             arr[i]=temp;
+
+//         }
+//         for(int i:arr)
+//         {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
 class SelectionSort{
     public static void main(String args[]){
         int arr[]={96,88,45,109,39};
