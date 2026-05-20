f2965230ddf46cc8531d49c0cc6eb2a0a4ebe470
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-05-20 23:42:18 +0530

    majority element

---
+++ Array/Majaority.java
@@ -0,0 +1,32 @@
+class Majaority {
+
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+        Majaority obj = new Majaority();
+
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        int ans = obj.majorityElement(nums);
+
+        System.out.println("Majority Element: " + ans);
+    }
+}
