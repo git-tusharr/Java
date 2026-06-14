6a24b5abe0371913092fb4859611f88ef26a938f
Tushar Chourasiya <chourasiyatushar53@gmail.com>
2026-06-14 21:45:36 +0530

    reverse pairs dsa

---
+++ Array/ReversePairs.java
@@ -0,0 +1,60 @@
+public class ReversePairs {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 2, 3, 1};
+
+        Solution sol = new Solution();
+        System.out.println(sol.reversePairs(nums));
+    }
+}
+
+class Solution {
+    private int count = 0;
+
+    public int reversePairs(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return count;
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        countPairs(nums, left, mid, right);
+        merge(nums, left, mid, right);
+    }
+
+    private void countPairs(int[] nums, int left, int mid, int right) {
+        int j = mid + 1;
+
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
+            count += j - (mid + 1);
+        }
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= right) temp[k++] = nums[j++];
+
+        System.arraycopy(temp, 0, nums, left, temp.length);
+    }
+}
